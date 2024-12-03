4bb7d9554179e4629b7327b7d7621b47cd2153c7
Steven Khong <khongwh@gmail.com>
2024-12-03 16:02:33 +0800

    remove discord invalid link

--- src/app/components/SocialLinkGroup/SocialLinkGroup.tsx
+++ src/app/components/SocialLinkGroup/SocialLinkGroup.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import cls from "classnames";
-import { ReactComponent as Discord } from "./social-icons/discord.svg";
 import { ReactComponent as TwitterIcon } from "./social-icons/twitter.svg";
 
 const useStyles = makeStyles((theme) => ({
@@ -44,12 +43,6 @@ const SocialLinkGroup: React.FC<SocialLinkGroupProps> = (
     <Box {...rest} className={cls(classes.root, className)}>
       <Button href="https://twitter.com/ZilSwap">
         <TwitterIcon />
-      </Button>
-      <Button
-        href="https://discord.gg/zilswapio"
-        className={cls({ [classes.compact]: compact })}
-      >
-        <Discord />
       </Button>
     </Box>
   );
